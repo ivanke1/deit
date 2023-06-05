9b24be389581a1e0d74807eb57a01880ee02c959
ivanke1 <31837721+ivanke1@users.noreply.github.com>
2023-06-04 18:21:43 -0700

    Update imnetprune.py

--- imnetprune.py
+++ imnetprune.py
@@ -186,7 +186,7 @@ def get_args_parser():
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
     
     parser.add_argument('--mask', action='store_true')
-    parser.add_argument('--mask_sparsity', default=.5)
+    parser.add_argument('--mask_sparsity', default=.5, type=float)
     parser.add_argument('--mask_resume', action='store_true')
     parser.add_argument('--load_mask', action='store_true')
     
