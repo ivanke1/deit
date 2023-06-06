54ee7627c121c1226d629da204060aac2dac8e4f
ivanke1 <31837721+ivanke1@users.noreply.github.com>
2023-06-05 23:30:19 -0700

    Update prune.py

--- prune.py
+++ prune.py
@@ -451,6 +451,15 @@ def main(args):
             return
                 
     if args.eval:
+        # verification
+        total_zero = 0
+        total = 0
+        for name, mod in model.named_modules():
+            if(hasattr(mod, 'weight') and name != 'module.head'):
+                total_zero += float(torch.sum(mod.weight == 0))
+                total += float(mod.weight.nelement())
+        print("Sparsity: {:.2f}%".format(total_zero/total))
+        
         test_stats = evaluate(data_loader_val, model, device)
         print(f"Accuracy of the network on the {len(dataset_val)} test images: {test_stats['acc1']:.1f}%")
         return
