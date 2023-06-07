f71231d68bad7f1d6849d18844ee22608fe1fcb6
ivanke1 <31837721+ivanke1@users.noreply.github.com>
2023-06-07 16:42:00 -0700

    Update imnetprune.py

--- imnetprune.py
+++ imnetprune.py
@@ -227,9 +227,6 @@ def main(args):
         model_without_ddp = model.module
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('number of params:', n_parameters)
-    if not args.unscale_lr:
-        linear_scaled_lr = args.lr * args.batch_size * utils.get_world_size() / 512.0
-        args.lr = linear_scaled_lr
     optimizer = create_optimizer(args, model_without_ddp)
     loss_scaler = NativeScaler()
 
